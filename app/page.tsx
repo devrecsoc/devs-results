16c1d8360dcb519208e6c734f4d5641088bc6411
Chandhru-27 <chandhru.l2007@gmail.com>
2026-08-06 18:15:45 +0530

    feat: update background image source to optimized version

--- app/page.tsx
+++ app/page.tsx
@@ -120,7 +120,7 @@ export default function Home() {
 
       {/* Overlay Background Image */}
       <Image
-        src="/bg%201.png"
+        src="/bg-opt.webp"
         alt="Background"
         fill
         priority
